bb0008c28aee642b049e8d2f6de129ef426fde2f
Emmanuel Ulelu <uleluemmanuel@gmail.com>
2022-11-22 11:23:09 +0100

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
-import VideoDetail from './components/VideoDetail'
-import ChannelDetail from './components/ChannelDetail'
-import SearchFeed from './components/SearchFeed'
-import Navbar from './components/Navbar'
-import { Feed } from '@mui/icons-material'
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'
 
 
 const App = () => {
-  <BrowserRouter>
+  return (
+    <BrowserRouter>
     <Box sx={{ backgroundColor:'#000' }}>
       <Navbar />
       <Routes>
@@ -20,6 +17,7 @@ const App = () => {
       </Routes>
     </Box>
   </BrowserRouter>
+  )
 }
 
 export default App
